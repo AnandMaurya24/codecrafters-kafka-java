0bf6cb7ed438f311cda8b9ca0415e6bdba6036ea
Anand Maurya <anandmaurya294@gmail.com>
2026-09-13 11:30:05 +0530

    codecrafters submit [skip ci]

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -59,7 +59,7 @@ public class Main {
         body.write(ByteBuffer.allocate(2).putShort((short) 18).array()); // api_key: 18 (ApiVersions)
         body.write(ByteBuffer.allocate(2).putShort((short) 0).array());  // min_version
         body.write(ByteBuffer.allocate(2).putShort((short) 4).array());  // max_version
-        body.write(ByteBuffer.allocate(2).putShort((short) 75).array()); // api_key: 75 (DescribeTopicPartitions)
+        body.write(75); // api_key: 75 (DescribeTopicPartitions)
         body.write(ByteBuffer.allocate(2).putShort((short) 0).array());  // min_version
         body.write(ByteBuffer.allocate(2).putShort((short) 0).array());
         body.write(0);                                                  // TAG_BUFFER for this api_keys entry
