b7c510f816acdc10bfd94679cbcf815d3a72f76d
Anand Maurya <anandmaurya294@gmail.com>
2026-09-13 16:51:52 +0530

    codecrafters submit [skip ci]

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -45,27 +45,50 @@ public class Main {
 
         // ab tak 8 bytes padh liye (apiKey + apiVersion + correlationId)
         // baaki bacha hua request body (client_id, software name/version, tag_buffer) discard karo
-        int remaining = requestMessageSize - 8;
+        // int remaining = requestMessageSize - 8;
+        // if (remaining > 0) {
+        //   in.readNBytes(remaining);
+        // }
+        byte[] client_id = in.readNBytes(2);
+        byte[] contents = in.readNBytes(9);
+        byte[] tag_buffer = in.readNBytes(1);
+        int array_length = ByteBuffer.wrap(in.readNBytes(1)).getInt();
+        int topic_name_length = ByteBuffer.wrap(in.readNBytes(1)).getInt();
+        byte[] topic_name = in.readNBytes(topic_name_length - 1);
+        int remaining = requestMessageSize - 8 - 2 - 9 - 1 - 1 - (topic_name_length - 1);
         if (remaining > 0) {
           in.readNBytes(remaining);
-        }
+        } 
 
         short apiVersionValue = ByteBuffer.wrap(apiVersion).getShort();
         short errorCode = (apiVersionValue >= 0 && apiVersionValue <= 4) ? (short) 0 : (short) 35;
 
         ByteArrayOutputStream body = new ByteArrayOutputStream();
-        body.write(ByteBuffer.allocate(2).putShort(errorCode).array()); // error_code
-        body.write(3);                                                  // api_keys: COMPACT_ARRAY length (1 entry -> N+1)
-        body.write(ByteBuffer.allocate(2).putShort((short) 18).array()); // api_key: 18 (ApiVersions)
-        body.write(ByteBuffer.allocate(2).putShort((short) 0).array());  // min_version
-        body.write(ByteBuffer.allocate(2).putShort((short) 4).array());  // max_version
-        body.write(0);                                                  // TAG_BUFFER for this api_keys entry
-        body.write(ByteBuffer.allocate(2).putShort((short) 75).array());
-        body.write(ByteBuffer.allocate(2).putShort((short) 0).array());  // min_version
-        body.write(ByteBuffer.allocate(2).putShort((short) 0).array());
-        body.write(0);                                                  // TAG_BUFFER for this api_keys entry
-        body.write(ByteBuffer.allocate(4).putInt(0).array());            // throttle_time_ms
-        body.write(0);                                                  // TAG_BUFFER for the response body
+        body.write(0);                                   // tag_buffer
+        body.write(ByteBuffer.allocate(4).putInt(0).array()); //throttle_time_ms
+        body.write(2);  // topic_array
+        body.write(ByteBuffer.allocate(2).putShort((short) 03).array()); //error_code ==> UNKNOWN_TOPIC_OR_PARTITION
+        body.write(topic_name_length)                                    // name length:
+        body.write(ByteBuffer.allocate(topic_name_length - 1).putShort((short) topic_name).array()); // topic name 
+        body.write(ByteBuffer.allocate(16).putShort((short) 0).array()) //topic_id
+        body.write(ByteBuffer.allocate(2).putShort((short) 0).array()); //is_internal
+        body.write(1);                                                   // partitions array
+        body.write(ByteBuffer.allocate(4).putShort((short) 0).array()) //topic_authorized_operations
+        body.write(0);                                                   // tag_buffer
+        body.write(ByteBuffer.allocate(2).putShort((short) -1).array());
+        body.write(0);                                                   // tag_buffer
+        // body.write(ByteBuffer.allocate(2).putShort(errorCode).array()); // error_code
+        // body.write(3);                                                  // api_keys: COMPACT_ARRAY length (1 entry -> N+1)
+        // body.write(ByteBuffer.allocate(2).putShort((short) 18).array()); // api_key: 18 (ApiVersions)
+        // body.write(ByteBuffer.allocate(2).putShort((short) 0).array());  // min_version
+        // body.write(ByteBuffer.allocate(2).putShort((short) 4).array());  // max_version
+        // body.write(0);                                                  // TAG_BUFFER for this api_keys entry
+        // body.write(ByteBuffer.allocate(2).putShort((short) 75).array());
+        // body.write(ByteBuffer.allocate(2).putShort((short) 0).array());  // min_version
+        // body.write(ByteBuffer.allocate(2).putShort((short) 0).array());
+        // body.write(0);                                                  // TAG_BUFFER for this api_keys entry
+        // body.write(ByteBuffer.allocate(4).putInt(0).array());            // throttle_time_ms
+        // body.write(0);                                                  // TAG_BUFFER for the response body
         byte[] bodyBytes = body.toByteArray();
 
         int responseMessageSize = 4 + bodyBytes.length; // correlation_id + body
